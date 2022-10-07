d24fbd5bafdf67ca79d7c90a7493d0f2812003ba
Joshimore <egorladur@gmail.com>
2022-10-07 18:59:04 +0300

    first commit

---
+++ Task 4/ApplicationLicense.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class ApplicationLicense
+    {
+        string licenseCodeTrial;
+        string licenseCodeCommon;
+        string licenseCodePro;
+
+        public string LicenseCodeTrial
+        {
+            get { return licenseCodeTrial; }
+            set { licenseCodeTrial = value; }
+        }
+        public string LicenseCodeCommon
+        {
+            get { return licenseCodeCommon; }
+            set { licenseCodeCommon = value; }
+        }
+        public string LicenceCodePro
+        {
+            get { return licenseCodePro; }
+            set { licenseCodePro = value; }
+        }
+        public void AllowTrial()
+        {
+            Console.WriteLine("Trial version");
+            Console.WriteLine("\nSelect an action:");
+
+            Console.WriteLine("1 - <+>");
+            Console.WriteLine("2 - <->");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    double resultaddition;
+                    double Firstnumberaddition;
+                    double Secondnumberaddition;
+
+                    Console.WriteLine("Type the first digit:");
+                    Firstnumberaddition = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Type the second digit:");
+                    Secondnumberaddition = double.Parse(Console.ReadLine());
+                    
+                    resultaddition = (Firstnumberaddition + Secondnumberaddition);
+                    
+                    Console.WriteLine("Result:" + resultaddition);
+                    break;
+                case "2":
+                    double resultsubstraction;
+                    double Firstnumbersubstraction;
+                    double Secondnumbersubstraction;
+
+                    Console.WriteLine("Type the first digit:");
+                    Firstnumbersubstraction = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Type the second digit:");
+                    Secondnumbersubstraction = double.Parse(Console.ReadLine());
+
+                    resultsubstraction = (Firstnumbersubstraction - Secondnumbersubstraction);
+
+                    Console.WriteLine("Result:" + resultsubstraction);
+                    break;
+                default:
+                    Console.WriteLine("**INPUT ERROR**");
+                    break;
+            }
+        }
+
+        public void AllowCommon()
+        {
+            Console.WriteLine("Free version");
+            Console.WriteLine("In this version are available new functions like <*> & </>");
+            Console.WriteLine("Select an action:");
+
+            Console.WriteLine("1 - <+>");
+            Console.WriteLine("2 - <->");
+            Console.WriteLine("3 - <*>");
+            Console.WriteLine("4 - </>");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    double resultaddition;
+                    double Firstnumberaddition;
+                    double Secondnumberaddition;
+
+                    Console.WriteLine("Type the first digit:");
+                    Firstnumberaddition = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Type the second digit:");
+                    Secondnumberaddition = double.Parse(Console.ReadLine());
+
+                    resultaddition = (Firstnumberaddition + Secondnumberaddition);
+
+                    Console.WriteLine("Result:" + resultaddition);
+                    break;
+                case "2":
+                    double resultsubstraction;
+                    double Firstnumbersubstraction;
+                    double Secondnumbersubstraction;
+
+                    Console.WriteLine("Type the first digit:");
+                    Firstnumbersubstraction = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Type the second digit:");
+                    Secondnumbersubstraction = double.Parse(Console.ReadLine());
+
+                    resultsubstraction = (Firstnumbersubstraction - Secondnumbersubstraction);
+
+                    Console.WriteLine("Result: " + resultsubstraction);
+                    break;
+                case "3":
+                    double resultmultiplication;
+                    double Firstnumbermultiplication;
+                    double Secondnumbermultiplication;
+
+                    Console.WriteLine("Type the first digit:");
+                    Firstnumbermultiplication = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Type the second digit:");
+                    Secondnumbermultiplication = double.Parse(Console.ReadLine());
+
+                    resultmultiplication = (Firstnumbermultiplication - Secondnumbermultiplication);
+
+                    Console.WriteLine("Result: " + resultmultiplication);
+                    break;
+                case "4":
+                    double resultdivision;
+                    double Firstnumberdivision;
+                    double Secondnumberdivision;
+
+                    Console.WriteLine("Type the first digit:");
+                    Firstnumberdivision = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Type the second digit:");
+                    Secondnumberdivision = double.Parse(Console.ReadLine());
+
+                    resultdivision = (Firstnumberdivision - Secondnumberdivision);
+
+                    Console.WriteLine("Result: " + resultdivision);
+                    break;
+                default:
+                    Console.WriteLine("**INPUT ERROR**");
+                    break;
+            }
+        }
+        public void AllowPro()
+        {
+            Console.WriteLine("Pro version");
+        }
+    }
+}
---
+++ Task 4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\t\t--Pro calculator free--");
+            Console.WriteLine("\nTrial version is activated by default");
+            Console.WriteLine("\nTo open trial version of a calculator type - 1");
+            Console.WriteLine("To use an activasion code for Common and Pro versions type - 2");
+
+            
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ApplicationLicense applicationLicense01 = new ApplicationLicense();
+                    applicationLicense01.AllowTrial();
+                    break;
+                case "2":
+                    break;
+                default:
+                    Console.WriteLine("\n**INPUT ERROR**");
+                    break;
+            }
+        }
+    }
+}
