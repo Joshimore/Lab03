2865a3730458cfc38b10346fd5f0b2794e21588f
Joshimore <egorladur@gmail.com>
2022-10-08 07:27:25 +0300

    Task 1 added

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -10,6 +10,19 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+            Gtr34 gtr34 = new("Nissan GTR r34", 100000, 0, "Petrol", "Mechanical", "Tokyo");
+            gtr34.Information34();
+
+            Gtr35 gtr35 = new("Nissan GTR r35", 200000, 0, "Petrol", "Mechanical", "Hiroshima");
+            gtr35.Information35();
+
+            Gtr34 car1 = gtr35;
+            car1.Information34();
+
+            Gtr35 car2 = (Gtr35)car1;
+            car2.Information35();
+
+            Console.ReadLine();
         }
     }
 }
